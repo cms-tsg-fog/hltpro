5dc06c259df602d6f724c38806f5c984defbe2b9
Marino Missiroli <m.missiroli@cern.ch>
2019-11-25 09:22:05 +0000

    only code-formatting in nCoresOnly.sh

--- nCoresOnly.sh
+++ nCoresOnly.sh
@@ -5,26 +5,30 @@ n=$nTot
 
 #Second statement is a requirement that the argument be a positive integer
 if [[ $# -eq 1 && $1 =~ [0-9]+$ ]]; then
-n=$1
+  n=$1
 fi
 
 #Quarantine all cores
 sudo mv /etc/appliance/resources/idle/core* /etc/appliance/resources/quarantined/
 
 for i in `seq 1 $n`; do
-if [[ $i -gt $nTot ]]; then
-echo This machine only has $nTot cores
-n=$nTot
-break
-fi
-#Hyperthreaded cores are paired so start with the even ones
-if [[ $i -le $(( $nTot/2 )) ]]; then
-iCore=$(( ($i - 1)*2 ))
-#If asked for more than nTot/2 cores, use hyperthreading
-else
-iCore=$(( ($i - 1 - $nTot/2)*2 + 1 ))
-fi
-sudo mv /etc/appliance/resources/quarantined/core${iCore} /etc/appliance/resources/idle/ 2> /dev/null
+
+  if [[ $i -gt $nTot ]]; then
+    echo This machine only has $nTot cores
+    n=$nTot
+    break
+  fi
+
+  # Hyperthreaded cores are paired so start with the even ones
+  if [[ $i -le $(( $nTot/2 )) ]]; then
+    iCore=$(( ($i - 1)*2 ))
+  # If asked for more than nTot/2 cores, use hyperthreading
+  else
+    iCore=$(( ($i - 1 - $nTot/2)*2 + 1 ))
+  fi
+
+  sudo mv /etc/appliance/resources/quarantined/core${iCore} /etc/appliance/resources/idle/ 2> /dev/null
+
 done
 
 echo Hilton will run hltd with $n cores
