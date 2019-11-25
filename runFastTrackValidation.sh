2015b889921cdab011cebd1ee2627a93847ab0ba
Marino Missiroli <m.missiroli@cern.ch>
2019-11-25 09:20:16 +0000

    updated GT in runFastTrackValidation.sh

--- runFastTrackValidation.sh
+++ runFastTrackValidation.sh
@@ -26,7 +26,7 @@ testGT=106X_dataRun3_HLT_Candidate_2019_11_22_16_09_07
 # # VirginRaw
 # testMenu=/cdaq/special/2019/MWGR1/VirginRaw/VR_Random_TS2/HLT/V4
 # runNumber=331595
-# testGT=106X_dataRun3_HLT_HcalResp_SiPixelLAforWidth_v1
+# testGT=106X_dataRun3_HLT_Candidate_2019_11_22_16_09_07
 
 ###############################
 
