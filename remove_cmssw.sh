2be232474b59a45ccfc821e402d9bf22c91546b0
Marino Missiroli <m.missiroli@cern.ch>
2020-01-08 19:01:07 +0000

    updated {bootstrap,remove}_cmssw.sh scripts

--- remove_cmssw.sh
+++ remove_cmssw.sh
@@ -1,29 +1,5 @@
 #! /bin/bash
 
-# load installation environment from a bootstrapped area
-function load_bootstrap_environment() {
-  test -n "$1" || return 1
-
-  # set the environment
-  export http_proxy="http://cmsproxy.cms:3128/"
-  export https_proxy="https://cmsproxy.cms:3128/"
-  export NO_PROXY=".cms"
-  export VO_CMS_SW_DIR="$(readlink -f $1)"
-  export LANG="C"
-  test -n "$SCRAM_ARCH" || SCRAM_ARCH=slc6_amd64_gcc481
-  export SCRAM_ARCH
-
-  # check that the area has been bootstrapped
-  if ! [ -f "$VO_CMS_SW_DIR"/"$SCRAM_ARCH"/external/apt/*/etc/profile.d/init.sh ]; then
-    echo "Can only load the environment from an already bootstrapped area"
-    return 1
-  fi
-
-  source "$VO_CMS_SW_DIR"/"$SCRAM_ARCH"/external/apt/*/etc/profile.d/init.sh
-  apt-get update
-}
-
-# some git stuff
 # boostrap a new CMSSW area
 function bootstrap_cmssw() {
   test -n "$1" || return 1
@@ -34,48 +10,48 @@ function bootstrap_cmssw() {
   export NO_PROXY=".cms"
   export VO_CMS_SW_DIR="$(readlink -f $1)"
   export LANG="C"
-  test -n "$SCRAM_ARCH" || SCRAM_ARCH=slc6_amd64_gcc481
+
+  test -n "$SCRAM_ARCH" || SCRAM_ARCH=slc7_amd64_gcc820
   export SCRAM_ARCH
 
   # if necessary, bootstrap the area
-  if ! [ -f "$VO_CMS_SW_DIR"/"$SCRAM_ARCH"/external/apt/*/etc/profile.d/init.sh ]; then
+  if ! [ -f "$VO_CMS_SW_DIR"/"$SCRAM_ARCH"/external/rpm/*/etc/profile.d/init.sh ]; then
     mkdir -p "$VO_CMS_SW_DIR"
     echo "Downloading bootstrap script"
-    wget -q -O "$VO_CMS_SW_DIR"/bootstrap.sh http://cmsrep.cern.ch/cmssw/cms/bootstrap.sh
-    chmod +x "$VO_CMS_SW_DIR"/bootstrap.sh
-
+    wget -O $VO_CMS_SW_DIR/bootstrap.sh http://cmsrep.cern.ch/cmssw/bootstrap.sh
     echo "Bootstrapping software area at $VO_CMS_SW_DIR"
-    "$VO_CMS_SW_DIR"/bootstrap.sh setup -path "$VO_CMS_SW_DIR" -arch "$SCRAM_ARCH" 2>&1 | tee "$VO_CMS_SW_DIR"/bootstrap_"$SCRAM_ARCH".log
+    sh -ex $VO_CMS_SW_DIR/bootstrap.sh setup -path $VO_CMS_SW_DIR -arch $SCRAM_ARCH >& $VO_CMS_SW_DIR/bootstrap_$SCRAM_ARCH.log
     if (( $? )); then
       echo
       echo "Bootstrap failed"
       return 1
     fi
-    echo "Acquire::Retries 99;" >> "$VO_CMS_SW_DIR"/"$SCRAM_ARCH"/external/apt/*/etc/apt.conf
-
-    # make the IBs available
-    echo "rpm http://cmsrep.cern.ch cmssw/cms.week0/apt/$SCRAM_ARCH lcg cms external"       >> "$VO_CMS_SW_DIR"/"$SCRAM_ARCH"/external/apt/*/etc/sources.list
-    echo "# rpm-src http://cmsrep.cern.ch cmssw/cms.week0/apt/$SCRAM_ARCH lcg cms external" >> "$VO_CMS_SW_DIR"/"$SCRAM_ARCH"/external/apt/*/etc/sources.list
-    echo "rpm http://cmsrep.cern.ch cmssw/cms.week1/apt/$SCRAM_ARCH lcg cms external"       >> "$VO_CMS_SW_DIR"/"$SCRAM_ARCH"/external/apt/*/etc/sources.list
-    echo "# rpm-src http://cmsrep.cern.ch cmssw/cms.week1/apt/$SCRAM_ARCH lcg cms external" >> "$VO_CMS_SW_DIR"/"$SCRAM_ARCH"/external/apt/*/etc/sources.list
   fi
 
   # optionally, install a CMSSW release
   test -n "$2" || return 0
-  echo "Updataing package database"
-  source "$VO_CMS_SW_DIR"/"$SCRAM_ARCH"/external/apt/*/etc/profile.d/init.sh
-  apt-get update
+
+  echo "Updating package database"
+  $VO_CMS_SW_DIR/common/cmspkg -a $SCRAM_ARCH update
+
   if echo $2 | grep -q patch; then
-    echo "Installing patch release $2"
-    apt-get -y -D remove cms+cmssw-patch+$2
-  elif echo $3 | grep -q ib; then
-    echo "Installing IB release $2"
-    apt-get -y -D remove cms+cmssw-ib+$2
+    echo "Removing patch release $2"
+    $VO_CMS_SW_DIR/common/cmspkg -a $SCRAM_ARCH remove -y cms+cmssw-patch+$2
+  elif echo $2 | grep -q ib; then
+    echo "Removing IB release $2"
+    $VO_CMS_SW_DIR/common/cmspkg -a $SCRAM_ARCH remove -y cms+cmssw-ib+$2
   else
-    echo "Installing release $2"
-    apt-get -y -D remove cms+cmssw+$2
+    echo "Removing release $2"
+    $VO_CMS_SW_DIR/common/cmspkg -a $SCRAM_ARCH remove -y cms+cmssw+$2
   fi
-  apt-get clean
+
+  echo "Cleaning package database"
+  $VO_CMS_SW_DIR/common/cmspkg -a $SCRAM_ARCH clean
 }
+
+if [ "$1" = "-a" ]; then
+  export SCRAM_ARCH="$2"
+  shift 2
+fi
 
 bootstrap_cmssw "$@"
