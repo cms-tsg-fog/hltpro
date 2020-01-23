a25266b4faed7ac77116d412accfd8e05c684304
Marino Missiroli <m.missiroli@cern.ch>
2020-01-23 11:54:45 +0000

    updated and re-tested {bootstrap,remove}_cmssw.sh scripts

--- bootstrap_cmssw.sh
+++ bootstrap_cmssw.sh
@@ -4,11 +4,16 @@
 function bootstrap_cmssw() {
   test -n "$1" || return 1
 
+  if [ ! -z "${CMSSW_BASE}" ]; then
+    echo "Bootstrap failed: a CMSSW area is already set up, will not bootstrap a new software area --> start from a clean session"
+    return 1
+  fi
+
   # set the environment
   export http_proxy="http://cmsproxy.cms:3128/"
   export https_proxy="https://cmsproxy.cms:3128/"
   export NO_PROXY=".cms"
-  export VO_CMS_SW_DIR="$(readlink -f $1)"
+  export VO_CMS_SW_DIR="$(readlink -m $1)"
   export LANG="C"
 
   test -n "$SCRAM_ARCH" || SCRAM_ARCH=slc7_amd64_gcc820
@@ -17,13 +22,13 @@ function bootstrap_cmssw() {
   # if necessary, bootstrap the area
   if ! [ -f "$VO_CMS_SW_DIR"/"$SCRAM_ARCH"/external/rpm/*/etc/profile.d/init.sh ]; then
     mkdir -p "$VO_CMS_SW_DIR"
+    rc=$?; if [[ $rc != 0 ]]; then return $rc; fi
     echo "Downloading bootstrap script"
     wget -O $VO_CMS_SW_DIR/bootstrap.sh http://cmsrep.cern.ch/cmssw/bootstrap.sh
-    echo "Bootstrapping software area at $VO_CMS_SW_DIR"
+    echo "Bootstrapping software area at $VO_CMS_SW_DIR (SCRAM_ARCH=${SCRAM_ARCH})"
     sh -ex $VO_CMS_SW_DIR/bootstrap.sh setup -path $VO_CMS_SW_DIR -arch $SCRAM_ARCH >& $VO_CMS_SW_DIR/bootstrap_$SCRAM_ARCH.log
     if (( $? )); then
-      echo
-      echo "Bootstrap failed"
+      printf "\n%s\n" "Bootstrap failed (dir=${VO_CMS_SW_DIR}, SCRAM_ARCH=${SCRAM_ARCH})"
       return 1
     fi
 #    sh -ex $VO_CMS_SW_DIR/bootstrap.sh reseed -path $VO_CMS_SW_DIR -arch $SCRAM_ARCH >& $VO_CMS_SW_DIR/bootstrap_$SCRAM_ARCH.log
