a5d940b6ca545a0be810d4dfe53323fbcd8b2f4c
Marino Missiroli <m.missiroli@cern.ch>
2020-01-24 17:03:00 +0000

    updated runFastTrackValidation.sh

--- runFastTrackValidation.sh
+++ runFastTrackValidation.sh
@@ -24,7 +24,7 @@ runNumber=331483
 testGT=106X_dataRun3_HLT_v4
 
 # # VirginRaw
-# testMenu=/cdaq/special/2019/MWGR1/VirginRaw/VR_Random_TS2/HLT/V5
+# testMenu=/cdaq/special/2019/MWGR1/VirginRaw/VR_Random_TS2/HLT/V6
 # runNumber=331595
 # testGT=106X_dataRun3_HLT_v4
 
