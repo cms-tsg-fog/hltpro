d9f2259715a290fd3d1fa63d5635fa95b82b7b69
Marino Missiroli <m.missiroli@cern.ch>
2020-02-04 12:38:41 +0000

    updated FRD2RAW.py

--- FRD2RAW.py
+++ FRD2RAW.py
@@ -1,37 +1,40 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("LHC")
+# example: cmsRun FRD2RAW.py inputFiles=file:fedraw1.raw,file:fedraw2.raw [outputFile=output.root] [run=0] [firstLS=1] [maxEvents=-1]
 
+process = cms.Process('LHC')
 
 import FWCore.ParameterSet.VarParsing as VarParsing
 options = VarParsing.VarParsing ('analysis') 
-options.register('firstRun',0,options.multiplicity.singleton,options.varType.int,"pass to firstRun")
+options.register('run', 0, options.multiplicity.singleton, options.varType.int, 'run number (passed to process.source.firstRun)')
+options.register('firstLS', 1, options.multiplicity.singleton, options.varType.int, 'first luminosity block for selected run')
 options.parseArguments()
 
+if len(options.inputFiles) == 0:
+   raise RuntimeError('empty list of input files (use "inputFiles=..")')
+
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+  input = cms.untracked.int32(options.maxEvents)
 )
 
-process.source = cms.Source("ErrorStreamSource",
-#                            fileNames = cms.untracked.vstring("/store/error_stream/run320917/run320917_ls0149_index000167_fu-c2d44-34-03_pid22180.raw"),
-                            fileNames = cms.untracked.vstring(options.inputFiles),
-                            firstRun  = cms.untracked.uint32(options.firstRun)
-                            )
-
+process.source = cms.Source('ErrorStreamSource',
+  fileNames = cms.untracked.vstring(options.inputFiles),
+  firstRun = cms.untracked.uint32(options.run),
+  firstLuminosityBlockForEachRun = cms.untracked.VLuminosityBlockID(*[cms.LuminosityBlockID(options.firstLS, options.run)]),
+)
 
 from EventFilter.RawDataCollector.rawDataCollectorByLabel_cfi import rawDataCollector
 process.rawDataCollector = rawDataCollector.clone(
-        verbose = cms.untracked.int32(0),
-            RawCollectionList = cms.VInputTag( cms.InputTag('source') )
-        )
+  verbose = cms.untracked.int32(0),
+  RawCollectionList = cms.VInputTag(cms.InputTag('source'))
+)
 
-process.output = cms.OutputModule( "PoolOutputModule",
-#                                   fileName = cms.untracked.string( "file:/cmsnfshltdata/hltdata/TSG/ErrorStream/test.root" ),
-                                   fileName = cms.untracked.string(options.outputFile ),
-                                   outputCommands = cms.untracked.vstring(
-                                                                          'drop *',
-                                                                          'keep *_rawDataCollector_*_*'
-                                   )
+process.output = cms.OutputModule('PoolOutputModule',
+  fileName = cms.untracked.string(options.outputFile),
+  outputCommands = cms.untracked.vstring(
+    'drop *',
+    'keep *_rawDataCollector_*_*',
+  ),
 )
 
 process.raw = cms.Path( process.rawDataCollector )
