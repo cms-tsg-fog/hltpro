51a1b181bdb579eeae030c9daf607d258e6e967d
hltpro <cmstsgfog@gmail.com>
2021-03-04 16:35:17 +0000

    Update maxEvents to 400

--- hltpro/genTestFakeBuFromRAW_cfg.py
+++ hltpro/genTestFakeBuFromRAW_cfg.py
@@ -245,7 +245,7 @@ elif len(fileNamesByRun_dict[options.runNumber]) == 0:
 process = cms.Process("FAKEBU")
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(400)
     #input = cms.untracked.int32(1000)
     #input = cms.untracked.int32(10000)
 )
