2c613f076e89cd285858210f86aa133b234b411b
Mateusz Zarucki <mateusz.zarucki@cern.ch>
2023-10-17 16:51:22 +0000

    Update event contents for HI collisions

--- hltpro/eventContent.py
+++ hltpro/eventContent.py
@@ -1,3 +1,5 @@
+# required event contents for different menu types
+
 import copy
 full = [
         'keep GlobalObjectMapRecord_hltGtStage2ObjectMap_*_*',
@@ -9,7 +11,9 @@ full = [
 # core content
 core_content = {
         'A': full,
+
         'B': full,
+
         'AlcaPi0': [
           'keep *_hltAlCaEtaEBUncalibrator_*_*',
           'keep *_hltAlCaEtaEEUncalibrator_*_*',
@@ -19,42 +23,50 @@ core_content = {
           'keep *_hltAlCaPi0RecHitsFilterEEonly_pi0EcalRecHitsES_*',
           'keep *_hltGtStage2Digis_*_*',
           'keep edmTriggerResults_*_*_*'],
+
         'AlcaPhiSym': [
           'keep *_hltAlCaPhiSymUncalibrator_*_*',
           'keep *_hltGtStage2Digis_*_*',
           'keep edmTriggerResults_*_*_*',
           'keep triggerTriggerEvent_*_*_*'],
+
         'AlcaLumiPixel': [
           'keep *_hltFEDSelectorLumiPixels_*_*',
           'keep *_hltGtStage2Digis_*_*',
           'keep edmTriggerResults_*_*_*'],
+
        'Calibration': [
           'keep *_hltEcalCalibrationRaw_*_*',
           'keep *_hltHcalCalibrationRaw_*_*',
           'keep edmTriggerResults_*_*_*',
           'keep triggerTriggerEvent_*_*_*'],
+
         'DQMOnlineBeamspot': [
           'keep *_hltFEDSelectorTCDS_*_*',
-          'keep edmTriggerResults_*_*_*',
-          'keep recoTracks_hltPFMuonMerging_*_*',
-          'keep recoVertexs_hltVerticesPFFilter_*_*'],
+          'keep edmTriggerResults_*_*_*'],
+
         'DQM_PA': full,
+
         'EcalCalibration': [
           'keep *_hltEcalCalibrationRaw_*_*',
           'keep edmTriggerResults_*_*_*',
           'keep triggerTriggerEvent_*_*_*'],
+
         'Express': full,
-        'HLTDQM':  [
+
+        'HLTDQM': [
           'keep *_hltTriggerSummaryAOD_*_*',
           'keep DcsStatuss_hltScalersRawToDigi_*_*',
           'keep *_hltGtStage2Digis_*_*',
           'keep LumiScalerss_hltScalersRawToDigi_*_*',
           'keep edmTriggerResults_*_*_*'],
-        'NanoDST':[
+
+        'NanoDST': [
           'keep *_hltFEDSelectorTCDS_*_*',
           'keep *_hltGtStage2Digis_*_*',
           'keep edmTriggerResults_*_*_*'],
-        'PhysicsDST':[
+
+        'PhysicsDST': [
           'keep *_hltActivityPhotonClusterShape_*_*',
           'keep *_hltActivityPhotonEcalIso_*_*',
           'keep *_hltActivityPhotonHcalForHE_*_*',
@@ -72,7 +84,8 @@ core_content = {
           'keep *_hltRecoEcalSuperClusterActivityCandidate_*_*',
           'keep *_hltGtStage2Digis_*_*',
           'keep edmTriggerResults_*_*_*'],
-        'RPCMON':[
+
+        'RPCMON': [
           'keep *_hltCscSegments_*_*',
           'keep *_hltDt4DSegments_*_*',
           'keep *_hltMuonCSCDigis_MuonCSCStripDigi_*',
@@ -89,10 +102,12 @@ core_content = {
           'keep *_hltMuonGEMDigis_AMCStatus_*',
           'keep *_hltMuonGEMDigis_OHStatus_*',
           'keep *_hltMuonGEMDigis_VFATStatus_*'],
-        'TrackerCalibration':[
+
+        'TrackerCalibration': [
           'keep *_hltTrackerCalibrationRaw_*_*',
           'keep edmTriggerResults_*_*_*',
           'keep triggerTriggerEvent_*_*_*'],
+
         'DQM': [
           'keep *_hltOnlineBeamSpot_*_*',
           'keep *_hltPixelTracks_*_*',
@@ -108,7 +123,7 @@ core_content = {
 extra_content = {}
 
 extra_content['collisions'] = {
-        'DQM':[
+        'DQM': [
           'keep *_hltL3NoFiltersNoVtxMuonCandidates_*_*',
           'keep *_hltPFJetForBtag_*_*',
           'keep *_hltSelector8CentralJetsL1FastJet_*_*',
@@ -125,17 +140,55 @@ extra_content['collisions'] = {
           'keep *_hltEgammaCandidates_*_*',
           'keep *_hltPixelVertices_*_*',
           'keep *_hltMergedTracks_*_*'],
+
         'DQMOnlineBeamspot': [
-          'keep *_hltFEDSelectorOnlineMetaData_*_*']
+          'keep recoTracks_hltPFMuonMerging_*_*',
+          'keep recoVertexs_hltVerticesPFFilter_*_*',
+          'keep *_hltFEDSelectorOnlineMetaData_*_*'],
        }
 
-extra_content['cosmics'] = {}
+extra_content['collisionsHI'] = {
+        'DQM': [
+          'keep *_hltL3NoFiltersNoVtxMuonCandidates_*_*',
+          'keep *_hltPFJetForBtag_*_*',
+          'keep *_hltSelector8CentralJetsL1FastJet_*_*',
+          'keep *_hltSiStripRawToClustersFacility_*_*',
+          'keep *_hltHoreco_*_*',
+          'keep *_hltSiPixelClustersCache_*_*',
+          'keep *_hltDeepCombinedSecondaryVertexBJetTagsPF_*_*',
+          'keep *_hltDeepCombinedSecondaryVertexBJetTagsCalo_*_*',
+          'keep *_hltHfreco_*_*',
+          'keep *_hltHbhereco_*_*',
+          'keep *_hltEcalRecHit_*_*',
+          'keep *_hltIter0HighPtTkMuTrackSelectionHighPurity_*_*',
+          'keep *_hltEgammaGsfElectrons_*_*',
+          'keep *_hltEgammaCandidates_*_*',
+          'keep *_hltPixelVertices_*_*',
+          'keep *_hltMergedTracks_*_*',
+
+          # new for HI
+          'keep *_hltSiStripClusterizerForRawPrime_*_*',
+          'keep *_hltSiStripClusters2ApproxClusters_*_*',
+          'keep FEDRawDataCollection_rawDataRepacker_*_*',
+          'keep DetIds_hltSiStripRawToDigi_*_*',
+          'keep *_hltSiStripClusters2ApproxClusters_*_*'],
+
+        'DQMOnlineBeamspot': [
+          'keep recoVertexs_hltVerticesPFFilterPPOnAA_*_*', # new for HI
+          'keep recoTracks_hltPFMuonMergingPPOnAA_*_*', # new for HI
+          'keep *_hltFEDSelectorOnlineMetaData_*_*'],
+       }
+
+extra_content['cosmics'] = {
+        'DQMOnlineBeamspot': [
+          'keep recoTracks_hltPFMuonMerging_*_*',
+          'keep recoVertexs_hltVerticesPFFilter_*_*'],
+}
 
 # final event content
 event_content = {}
 
-event_content['collisions']   = {key:core_content.get(key,[]) + extra_content['collisions'].get(key,[]) for key in set(list(core_content.keys()) + list(extra_content['collisions'].keys()))}
-event_content['collisionsHI'] = event_content['collisions']
+for menuType in ['collisions', 'collisionsHI', 'cosmics']: 
+    event_content[menuType] = {key:core_content.get(key,[]) + extra_content[menuType].get(key,[]) for key in set(list(core_content.keys()) + list(extra_content[menuType].keys()))}
 
-event_content['cosmics']      = {key:core_content.get(key,[]) + extra_content['cosmics'].get(key,[])    for key in set(list(core_content.keys()) + list(extra_content['cosmics'].keys()))}
-event_content['circulating']  = event_content['cosmics']
+event_content['circulating'] = event_content['cosmics']
