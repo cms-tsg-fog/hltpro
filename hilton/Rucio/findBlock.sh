df5284f9a133314b342a372893a8772b30f0607a
Mateusz Zarucki <mateusz.zarucki@cern.ch>
2024-03-27 15:42:39 +0100

    Move Rucio scripts to cms-tsg/fog/scripts

--- hilton/Rucio/findBlock.sh
+++
@@ -1,2 +0,0 @@
-file=/store/data/Run2022A/MinimumBias0/RAW/v1/000/352/568/00000/d4dcc6b1-0c5d-4b89-a117-d82fbceec7d1.root
-rucio list-parent-dids cms:$file
--- hilton/Rucio/setupRucio.sh
+++
@@ -1,4 +0,0 @@
-source /cvmfs/cms.cern.ch/cmsset_default.sh &&
-source /cvmfs/cms.cern.ch/rucio/setup-py3.sh &&
-export RUCIO_ACCOUNT="t2_ch_cern_local_users" &&
-voms-proxy-init -voms cms -rfc -valid 192:00
